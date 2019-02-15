217900170a1612b12813c9fa534742764058a9e7
PolinaAlexandr <polinaalexnder@gmail.com>
2019-02-15 18:13:01 +0300

    Fixed directory with .git showing

--- console_tree.py
+++ console_tree.py
@@ -11,17 +11,20 @@ class GitRepo:
         # TODO: add repo statistic
         # self.commit_count = .....
 
+
+# while True:
 root_dir = os.path.join(sys.argv[1])\
     if len(sys.argv) > 1 else \
     os.path.expanduser('~')
 
-git_repos = list()
 
-for root, dirs, files in os.walk(root_dir):
-    if '.git' in dirs:
-        new_repo = GitRepo(os.path.abspath(root))
-        git_repos.append(new_repo)
-        break
+git_repos = list()
+# while True:
+for root, dirs, files in os.walk(root_dir, topdown=True):
+    dirs[:] = [ dir for dir in dirs if dir == '.git']
+    new_repo = GitRepo(os.path.abspath(root))
+    git_repos.append(new_repo)
+        # break
 
 for git_repo in git_repos:
     print(git_repo.repo_path)
