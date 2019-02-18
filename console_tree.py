f6ce07f69026460831834de85defc2c1c028b8fa
PolinaAlexandr <polinaalexnder@gmail.com>
2019-02-18 14:09:41 +0300

    Fixed git research

--- console_tree.py
+++ console_tree.py
@@ -12,19 +12,16 @@ class GitRepo:
         # self.commit_count = .....
 
 
-# while True:
 root_dir = os.path.join(sys.argv[1])\
     if len(sys.argv) > 1 else \
     os.path.expanduser('~')
 
-
 git_repos = list()
-# while True:
 for root, dirs, files in os.walk(root_dir, topdown=True):
-    dirs[:] = [ dir for dir in dirs if dir == '.git']
-    new_repo = GitRepo(os.path.abspath(root))
-    git_repos.append(new_repo)
-        # break
+    if '.git' in dirs:
+        dirs = []
+        new_repo = GitRepo(os.path.abspath(root))
+        git_repos.append(new_repo)
 
 for git_repo in git_repos:
     print(git_repo.repo_path)
