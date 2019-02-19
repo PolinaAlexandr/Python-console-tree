ecc35ccf5ac0cd88ccab639a4ec7dba49e06141a
PolinaAlexandr <polinaalexnder@gmail.com>
2019-02-20 00:10:20 +0300

    Added commit count to the GitRepo class

--- console_tree.py
+++ console_tree.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 import os
 import sys
+import subprocess
 
 
 class GitRepo:
     
-    def __init__(self, repo_path):
+    def __init__(self, repo_path, commit_count):
         self.repo_path = repo_path
-
-        # TODO: add repo statistic
+        self.commit_count = commit_count
+        # TODO: add repo statistic 
         # self.commit_count = .....
+        
+    def __str__(self):
+        return f'Path: {self.repo_path} Commit count: {self.commit_count}'
+
 
 
 root_dir = os.path.join(sys.argv[1])\
@@ -20,8 +25,12 @@ git_repos = list()
 for root, dirs, files in os.walk(root_dir, topdown=True):
     if '.git' in dirs:
         dirs = []
-        new_repo = GitRepo(os.path.abspath(root))
+        os.chdir(root)
+        commit_count = subprocess.run(['git', 'rev-list', '--count', '--all'], stdout=subprocess.PIPE).stdout
+        new_repo = GitRepo(os.path.abspath(root), int(commit_count.decode().strip()))
         git_repos.append(new_repo)
 
-for git_repo in git_repos:
-    print(git_repo.repo_path)
+
+
+for i, git_repo in enumerate(git_repos):
+    print(f'{i + 1}. {git_repo}')
